f75b1bb0bc9ed24943fdf8a9bda5ba20dd9dadb5
matheuspaz123 <matheuspaz30071998@gmail.com>
2020-06-17 23:22:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/themovie/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.themovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //api_key = ec69d673e213c2849cd207f63b94a622
+    }
+}
